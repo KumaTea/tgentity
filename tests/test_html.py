29007afc15f3ae860ff51902340e653b0ca0d480
KumaTea <KumaTea@outlook.com>
2023-11-08 05:56:55 +0800

    test

---
+++ tests/test_html.py
@@ -0,0 +1,54 @@
+import unittest
+from tgentity import to_html
+from tgentity.types import Message, MessageEntity
+
+
+HTML_test = "URL entity inside bold must stringify to HTML correctly"
+
+
+class TestHTML(unittest.TestCase):
+    print("Testing HTML")
+
+    def test_html(self):
+        assert to_html(
+            Message(
+                id=0,
+                text="Hello, https://feathers.studio!",
+                entities=[
+                    MessageEntity(type="bold", offset=0, length=len("Hello, https://feathers.studio!")),
+                    MessageEntity(type="url", offset=len("Hello, "), length=len("https://feathers.studio")),
+                ]
+            )
+        ) == '<b>Hello, <a href="https://feathers.studio">https://feathers.studio</a>!</b>', HTML_test
+
+        assert to_html(
+            Message(
+                id=0,
+                text="Hello, feathers.studio!",
+                entities=[
+                    MessageEntity(type="bold", offset=0, length=len("Hello, feathers.studio!")),
+                    MessageEntity(
+                        type="text_link",
+                        offset=len("Hello, "),
+                        length=len("feathers.studio"),
+                        url="https://feathers.studio"
+                    ),
+                ]
+            )
+        ) == '<b>Hello, <a href="https://feathers.studio">feathers.studio</a>!</b>', HTML_test
+
+        assert to_html(
+            Message(
+                id=0,
+                text="👉 Link :- https://example.com?x&y",
+                entities=[
+                    MessageEntity(offset=0, length=10, type="bold"),
+                    MessageEntity(offset=10, length=23, type="url"),
+                    MessageEntity(offset=10, length=23, type="bold"),
+                ]
+            )
+        ) == '<b>👉 Link :- </b><a href="https://example.com?x&y"><b>https://example.com?x&amp;y</b></a>', HTML_test
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_md.py
@@ -0,0 +1,53 @@
+import unittest
+from tgentity import to_markdown_v2
+from tgentity.types import Message, MessageEntity
+
+
+MarkdownV2_test = "URL entity inside bold must stringify to Markdown correctly"
+
+
+class TestMarkdownV2(unittest.TestCase):
+    print("Testing Markdown V2")
+
+    def test_markdown_v2(self):
+        assert to_markdown_v2(
+            Message(
+                id=0,
+                text="Hello, https://feathers.studio!",
+                entities=[
+                    MessageEntity(type="bold", offset=0, length=len("Hello, https://feathers.studio!")),
+                    MessageEntity(type="url", offset=len("Hello, "), length=len("https://feathers.studio")),
+                ]
+            )
+        ) == "*Hello, https://feathers\\.studio\\!*", MarkdownV2_test
+
+        assert to_markdown_v2(
+            Message(
+                id=0,
+                text="Hello, feathers.studio!",
+                entities=[
+                    MessageEntity(type="bold", offset=0, length=len("Hello, feathers.studio!")),
+                    MessageEntity(
+                        type="text_link",
+                        offset=len("Hello, "),
+                        length=len("feathers.studio"),
+                        url="https://feathers.studio"),
+                ]
+            )
+        ) == "*Hello, [feathers\\.studio](https://feathers.studio)\\!*", MarkdownV2_test
+
+        assert to_markdown_v2(
+            Message(
+                id=0,
+                text="👉 Link :- https://example.com",
+                entities=[
+                    MessageEntity(type="bold", offset=0, length=10),
+                    MessageEntity(type="url", offset=10, length=19),
+                    MessageEntity(type="bold", offset=10, length=19),
+                ]
+            )
+        ) == "*👉 Link :\\- **https://example\\.com*", MarkdownV2_test
+
+
+if __name__ == "__main__":
+    unittest.main()
